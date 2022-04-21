9737b920c5120c7fdae4c77e7c395db3c957cbc7
Max <maxime.michel03@protonmail.com>
2022-04-21 11:01:55 +0200

    add movie filter

--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -1,23 +1,36 @@
 <template>
-  <MovieCard v-for="(movie, index) in testapi" :key="`movie-${index}`" :movie="movie" />
+  <input type="text" v-model="filter" />
+  <v-text-field label="Another input"></v-text-field>
+  <MovieCard
+    v-for="(movie, index) in testapi"
+    :key="`movie-${index}`"
+    :movie="movie"
+  />
 </template>
 
 <script>
-import MovieCard from '@/components/MovieCard.vue'
+import MovieCard from "@/components/MovieCard.vue";
 
 export default {
   components: { MovieCard },
-  data () {
+  data() {
     return {
-      testapi: []
-    }
+      testapi: [],
+      filter: "",
+    };
   },
-  async mounted () {
-    this.testapi = await this.$Movie.getPopularMovies()
-  }
-}
+  async mounted() {
+    this.testapi = await this.$Movie.getPopularMovies();
+  },
+  watch: {
+    filter: async function () {
+      var movies = await this.$Movie.getPopularMovies();
+      this.testapi = movies.filter((dt) =>
+        dt.title.match(new RegExp(this.filter, "i"))
+      );
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
