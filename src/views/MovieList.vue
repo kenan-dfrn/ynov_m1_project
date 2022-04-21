86f7940cd2c8b455b0a0eb1b0f0491f42f5422a5
anebris <kenan.dufrene@live.fr>
2022-04-21 09:16:14 +0200

    deploy

---
+++ src/views/MovieList.vue
@@ -0,0 +1,24 @@
+<template>
+  <MovieCard v-for="index in 1/*(movie, index) in testapi.data*/" :key="`movie-${index}`" :movie="testapi.data" />
+</template>
+
+<script>
+import axios from 'axios'
+import MovieCard from '@/components/MovieCard.vue'
+
+export default {
+  components: { MovieCard },
+  data () {
+    return {
+      testapi: []
+    }
+  },
+  async mounted () {
+    this.testapi = await axios.get('https://api.themoviedb.org/3/movie/latest?api_key=5545b123bb0d696c917f4b88dd656165')
+  }
+}
+</script>
+
+<style>
+
+</style>
