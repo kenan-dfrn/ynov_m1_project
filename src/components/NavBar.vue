e96d294a5a545c0b7ccf7945571e956034b04935
Max <maxime.michel03@protonmail.com>
2022-04-28 11:44:51 +0200

    addNav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,98 @@
 <template>
-    <div class="navbar">
-        <GenreList :genres='genres' @searchByGenre='searchByGenre' />
+  <div class="w-100 sticky-top navbar">
+    <div class="navbar-container container w-100">
+      <div class="row justify-content-between w-100">
+        <div class="col-lg-9 w-100">
+          <nav class="d-flex w-100">
+            <h1 class="logo">
+              MOVIEFINDER<span style="color: #ff0000"
+                >Z<span style="color: #d10404">Z</span></span
+              ><span style="color: #b60000">Z</span>
+            </h1>
+            <div
+              class="w-50 navbar-items d-flex justify-content-start align-items-center"
+            >
+              <label class="label-navbar">Genre :</label>
+              <GenreList :genres="genres" @searchByGenre="searchByGenre" />
+              <label class="label-navbar">Rechercher :</label>
+              <input
+                class="form-input"
+                type="text"
+                v-model="filters.title"
+                placeholder="Rechercher un film"
+              />
+            </div>
+          </nav>
+        </div>
+      </div>
     </div>
-    
+  </div>
 </template>
 
 <script>
-import GenreList from './GenreList.vue'
+import GenreList from "./GenreList.vue";
 
 export default {
-    components : { GenreList },
-    props: { genres: { type: Object },
+  components: { GenreList },
+  props: { genres: { type: Object } },
+  methods: {
+    searchByGenre(genreId) {
+      this.$emit("searchByGenre", genreId);
     },
-    methods: {
-        searchByGenre (genreId) {
-            this.$emit('searchByGenre', genreId);
-        },
-    }
-}
+    searchWithFilter(filter) {
+      this.$emit("searchWithFilter", filter);
+    },
+  },
+  data() {
+    return {
+      filters: {
+        title: "",
+        genres: null,
+      },
+      movieList: [],
+    };
+  },
+  watch: {
+    "filters.title": async function (value) {
+      this.searchWithFilter(value);
+    },
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.navbar {
+  background-color: #272829 !important;
+}
+.navbar-container {
+  padding-left: 0% !important;
+}
+.logo {
+  margin-top: 8px;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+.label-navbar {
+  margin-right: 8px;
+  align-self: center;
+  font-size: 20px;
+}
+.form-input {
+  background-color: #474343;
+  border: 1px solid white !important;
+  color: white;
+  border-radius: 6px;
+}
+.form-input::placeholder {
+  color: white;
+}
+.form-input:focus {
+  outline: none !important ;
+}
+
+.navbar-items {
+  margin-left: 30px;
+}
+#logo-end {
+  color: #b60000;
+}
+</style>
