b11df57ccd56b80429caaf2119b4ec575816a2bd
Max <maxime.michel03@protonmail.com>
2022-04-28 10:49:03 +0200

    fix conflicts

--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -1,11 +1,16 @@
 <template>
-  <NavBar  :genres="genres" @searchByGenre="searchByGenre"/>
+  <NavBar :genres="genres" @searchByGenre="searchByGenre" />
   <MovieSlider
+    title="Popular Movies"
     :items="movieList.results"
     :genres="genres"
   />
-    title="Popular Movies"
-  <Pagination @updatePage="updatePage($event)" :page="lazy.params.page" :totalPages="movieList.total_pages"/>
+
+  <Pagination
+    @updatePage="updatePage($event)"
+    :page="lazy.params.page"
+    :totalPages="movieList.total_pages"
+  />
   <input type="text" v-model="lazy.params.title" />
 </template>
 
@@ -22,8 +27,8 @@ export default {
         params: {
           title: "",
           genres: null,
-          page: 0
-        }
+          page: 0,
+        },
       },
       movieList: [],
       genres: [],
@@ -31,41 +36,41 @@ export default {
   },
   async mounted() {
     this.movieList = await this.$Movie.getPopularMovies(this.lazy.params);
-    this.genres = await this.$Genre.getGenreList()
+    this.genres = await this.$Genre.getGenreList();
   },
   watch: {
     "lazy.params.title": async function () {
-      this.getMovieList()
+      this.getMovieList();
     },
     movieList: function (value) {
       console.log(value);
     },
   },
   methods: {
-     async searchByGenre (id) {
-        this.lazy.params.genre = id
-        this.getMovieList()
-     },
-     async updatePage (page) {
-        this.lazy.params.page = page
-        this.getMovieList()
-     },
-     async getMovieList () {
-       let movies = []
-       if (this.lazy.params.genre >= 0) {
-         movies = await this.$Movie.getMoviesByGenre(this.lazy.params);
-       } else {
-         movies = await this.$Movie.getPopularMovies(this.lazy.params);
-       }
-      
+    async searchByGenre(id) {
+      this.lazy.params.genre = id;
+      this.getMovieList();
+    },
+    async updatePage(page) {
+      this.lazy.params.page = page;
+      this.getMovieList();
+    },
+    async getMovieList() {
+      let movies = [];
+      if (this.lazy.params.genre >= 0) {
+        movies = await this.$Movie.getMoviesByGenre(this.lazy.params);
+      } else {
+        movies = await this.$Movie.getPopularMovies(this.lazy.params);
+      }
+
       if (this.lazy.params.title) {
         movies.results = movies.results.filter((dt) =>
           dt.title.match(new RegExp(this.lazy.params.title, "i"))
         );
       }
 
-      this.movieList = movies
-    }
+      this.movieList = movies;
+    },
   },
 };
 </script>
